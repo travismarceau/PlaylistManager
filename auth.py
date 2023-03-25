63d98f0e2ef6bf91c20585089faf8815630ad221
travismarceau <t@travismarceau.com>
2023-03-24 22:39:20 -0700

    device again

--- auth.py
+++ auth.py
@@ -8,36 +8,44 @@ CLIENT_SECRET = os.environ["CLIENT_SECRET"]
 REDIRECT_URI = os.environ["REDIRECT_URI"]
 SCOPE = 'playlist-modify-private'
 
-# User Authorization: Step 1 - Generate the authorization URL
-auth_url = "https://accounts.spotify.com/authorize"
-params = {
-    "client_id": CLIENT_ID,
-    "response_type": "code",
-    "redirect_uri": REDIRECT_URI,
-    "scope": SCOPE,
-}
-auth_url = f"{auth_url}?{urlencode(params)}"
-print(f"URL: {auth_url}")
-
-# User Authorization: Step 2 - User logs in and authorizes the app
-print("Please visit the following URL to authorize the app: \n")
-print(auth_url)
-auth_code = input("\nEnter the code received from the URL above: ")
-
-# Token Exchange: Step 1 - Exchange the authorization code for an access token
-token_url = "https://accounts.spotify.com/api/token"
+# Device Flow: Step 1 - Get the device code
+auth_url = "https://accounts.spotify.com/api/device_authorization"
 payload = {
-    "grant_type": "authorization_code",
-    "code": auth_code,
-    "redirect_uri": REDIRECT_URI,
+    "client_id": CLIENT_ID,
+    "scope": "playlist-modify-private",
 }
 
-response = requests.post(token_url, data=payload, auth=(CLIENT_ID, CLIENT_SECRET))
-print(f"Response status: {response.status_code}")
+response = requests.post(auth_url, data=payload)
+response_data = response.json()
 
-if response.status_code == 200:
-    token_data = response.json()
-    access_token = token_data["access_token"]
-    print(f"Access token: {access_token}")
-else:
-    print("Error: Unable to obtain access token.")
+device_code = response_data["device_code"]
+user_code = response_data["user_code"]
+verification_uri = response_data["verification_uri"]
+verification_uri_complete = response_data["verification_uri_complete"]
+expires_in = response_data["expires_in"]
+interval = response_data["interval"]
+
+print(f"Please visit the following URL to authorize the app: {verification_uri_complete}")
+
+# Device Flow: Step 2 - Poll for the access token
+token_url = "https://accounts.spotify.com/api/token"
+start_time = time.time()
+
+while True:
+    payload = {
+        "grant_type": "urn:ietf:params:oauth:grant-type:device_code",
+        "client_id": CLIENT_ID,
+        "device_code": device_code,
+    }
+    response = requests.post(token_url, data=payload, auth=(CLIENT_ID, CLIENT_SECRET))
+    
+    if response.status_code == 200:
+        token_data = response.json()
+        access_token = token_data["access_token"]
+        print(f"Access token: {access_token}")
+        break
+    elif response.status_code == 400 and time.time() < start_time + expires_in:
+        time.sleep(interval)
+    else:
+        print("Error: Unable to obtain access token.")
+        break
