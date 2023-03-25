790c3974214fd66003d06826fd4a401c343e4e9e
travismarceau <t@travismarceau.com>
2023-03-24 21:01:12 -0700

    days fix

--- playlist_manager.py
+++ playlist_manager.py
@@ -131,7 +131,7 @@ def handle_old_tracks(engine, sp, weekly_playlist_id, archive_playlist_id):
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    seven_days_ago = datetime.now() + timedelta(day=1)
+    seven_days_ago = datetime.now() + timedelta(days=1)
     # seven_days_ago = datetime.now() - timedelta(days=7)
     old_tracks = session.query(Weekly).filter(Weekly.add_date <= seven_days_ago).all()
 
